f8d7e9d2448f8d87d1982691423c86386452ac7f
Adebola Omoboya <adeomoboya@gmail.com>
2025-05-05 14:02:34 +0100

    Additional card in order details to accommodate customer instructions such as cut or not cut and any additional instructions

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -3,10 +3,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CustomizerSettingsService } from '../customizer-settings/customizer-settings.service';
-import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
 import {AuthService} from "../authentication/auth.service";
 import {AsyncPipe, NgIf, NgOptimizedImage} from "@angular/common";
-import {FilesAcceptDirective, FileUploadComponent} from "@iplab/ngx-file-upload";
 import {catchError} from "rxjs/operators";
 import {HttpErrorResponse} from "@angular/common/http";
 import {BehaviorSubject, finalize, throwError} from "rxjs";
@@ -23,14 +21,9 @@ import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
         MatCardModule,
         MatButtonModule,
         RouterLinkActive,
-        MatMenu,
-        MatMenuItem,
-        MatMenuTrigger,
         AsyncPipe,
         NgIf,
         NgOptimizedImage,
-        FileUploadComponent,
-        FilesAcceptDirective,
         MatProgressSpinner
     ],
     templateUrl: './settings.component.html',
@@ -46,7 +39,7 @@ export class SettingsComponent implements OnInit {
 
     constructor(
         public authService: AuthService,
-        private snackbarservice: SnackbarService,
+        private snackbarService: SnackbarService,
         public themeService: CustomizerSettingsService
     ) {
         this.themeService.isToggled$.subscribe(isToggled => {
@@ -61,11 +54,11 @@ export class SettingsComponent implements OnInit {
             finalize(() => { this.loadingSubject.next(false) }),
             catchError((error: HttpErrorResponse) => {
                 const message = 'Error removing Image';
-                this.snackbarservice.message(message);
+                this.snackbarService.message(message);
                 return throwError(() => new Error(message));
             })
         ).subscribe( result => {
-            this.snackbarservice.message('Image removed successfully');
+            this.snackbarService.message('Image removed successfully');
             this.authService.reloadUser();
         });
     }
@@ -83,11 +76,11 @@ export class SettingsComponent implements OnInit {
                 finalize(() => { this.loadingSubject.next(false) }),
                 catchError((error: HttpErrorResponse) => {
                     const message = 'Error uploading file make sure it is an image file and not larger than 2MB';
-                    this.snackbarservice.message(message);
+                    this.snackbarService.message(message);
                     return throwError(() => new Error(message));
                 })
             ).subscribe( result => {
-                this.snackbarservice.message('Image uploaded successfully');
+                this.snackbarService.message('Image uploaded successfully');
                 this.authService.reloadUser();
             });
         }
